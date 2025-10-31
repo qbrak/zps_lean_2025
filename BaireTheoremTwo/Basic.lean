39030994cbf13603e450a843d8c31f4b28b2ddb0
Piotr Held <pjheld@gmail.com>
2025-10-31 16:37:32 +0100

    Update changes before starting blueprint.

--- BaireTheoremTwo/Basic.lean
+++ BaireTheoremTwo/Basic.lean
@@ -3,6 +3,7 @@ import Mathlib.Topology.MetricSpace.Basic
 import Mathlib.Topology.Basic
 import Mathlib.Order.BooleanAlgebra.Basic
 import Mathlib.Topology.Closure
+import Mathlib.Topology.MetricSpace.Pseudo.Defs
 
 noncomputable section
 
@@ -16,7 +17,13 @@ section BaireTheoremTwo
 variable [TopologicalSpace X] [BaireSpace X]
 variable [TopologicalSpace Y] [MetricSpace Y] [CompleteSpace Y]
 
-theorem dense_set_intersect_open_nonempty (U : Set Y) (V : Set Y)
+
+/--
+Potentially could simplified with Dense.exists_mem_open which is:
+  (hs : Dense s) {U : Set X} (ho : IsOpen U) (hne : U.Nonempty) : ∃ x ∈ s, x ∈ U
+-/
+
+theorem dense_set_intersect_open_nonempty {U : Set X} {V : Set X}
      (hU : IsOpen U) (hne : U.Nonempty) (hd : Dense V) :
     (U ∩ V).Nonempty := by
   by_contra h_empty
@@ -54,6 +61,14 @@ theorem dense_set_intersect_open_nonempty (U : Set Y) (V : Set Y)
 
   contradiction
 
+theorem set_dense_iff_intersect_open_nonempty {s : Set X} :
+    Dense s ↔ ∀ (U : Set X), IsOpen U → U.Nonempty → (U ∩ s).Nonempty := by
+  exact dense_iff_inter_open
+
+theorem exist_open_ball_smaller_radius_subset {X : Set Y} {r : ℝ} (hr : 0 < r) (U : Set Y)
+    (hUopen : IsOpen U) (hXOpen : IsOpen X) (hXDense : Dense X) :
+    ∃ (x : Y) (r2 : ℝ), r2 > 0 ∧ (Metric.ball x r2 ⊆ U ∩ X) ∧ r2 < r := by
+  sorry
 
 
 theorem dense_cM_iInter_of_isOpen_nat {f : ℕ → Set Y} (ho : ∀ n, IsOpen (f n))
@@ -61,7 +76,6 @@ theorem dense_cM_iInter_of_isOpen_nat {f : ℕ → Set Y} (ho : ∀ n, IsOpen (f
     -- From topology without tears
     by
     intro U hUopen hUne
-
     sorry
 
 theorem dense_iInter_of_isOpen_nat {f : ℕ → Set X} (ho : ∀ n, IsOpen (f n))
