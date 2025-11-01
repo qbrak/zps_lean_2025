c604ba97e7cd6d8508f06e4a478936acb343403c
Monika Brattig <345051@uwr.edu.pl>
2025-11-01 21:12:17 +0100

    oui oui

--- BaireTheoremTwo/Basic.lean
+++ BaireTheoremTwo/Basic.lean
@@ -23,6 +23,8 @@ Potentially could simplified with Dense.exists_mem_open which is:
   (hs : Dense s) {U : Set X} (ho : IsOpen U) (hne : U.Nonempty) : ∃ x ∈ s, x ∈ U
 -/
 
+#check X
+
 theorem dense_set_intersect_open_nonempty {U : Set X} {V : Set X}
      (hU : IsOpen U) (hne : U.Nonempty) (hd : Dense V) :
     (U ∩ V).Nonempty := by
