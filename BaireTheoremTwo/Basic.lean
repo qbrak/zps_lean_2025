45346184932b1bee4d8107387a1c3deb45eeafd8
Monika Brattig <345051@uwr.edu.pl>
2025-11-12 16:47:18 +0100

    robaczek_swietojanski

--- BaireTheoremTwo/Basic.lean
+++ BaireTheoremTwo/Basic.lean
@@ -5,6 +5,7 @@ import Mathlib.Order.BooleanAlgebra.Basic
 import Mathlib.Topology.Closure
 import Mathlib.Topology.MetricSpace.Pseudo.Defs
 
+set_option linter.style.longLine false
 noncomputable section
 
 open scoped Topology
@@ -76,7 +77,7 @@ theorem set_dense_iff_intersect_open_nonempty {s : Set Y} :
 
 lemma exist_open_ball_smaller_radius_subset {P : Set Y} {r : ℝ} (hr : 0 < r) (U : Set Y)
     (hUopen : IsOpen U) (hUne : U.Nonempty) (hPOpen : IsOpen P) (hPDense : Dense P) :
-    ∃ (x : Y) (r2 : ℝ), r2 > 0 ∧ (Metric.ball x r2 ⊆ U ∩ P) ∧ r2 < r := by
+    ∃ (x : Y) (r2 : ℝ), r2 > 0 ∧ (Metric.closedBall x r2 ⊆ U ∩ P) ∧ r2 < r := by
     have h_inter_nonempty : (U ∩ P).Nonempty := by
       exact set_dense_iff_intersect_open_nonempty.mp hPDense U hUopen hUne
     have open_inter : IsOpen (U ∩ P) := by
@@ -89,18 +90,20 @@ lemma exist_open_ball_smaller_radius_subset {P : Set Y} {r : ℝ} (hr : 0 < r) (
       use ε
     rcases exists_any_ball with ⟨x0, g1, hx0, hg1, hg2⟩
     use x0
-    use min g1 (r / 2)
+    use min (g1/2) (r / 2)
     constructor
-    apply lt_min hg1 (half_pos hr)
-    constructor
-    apply Subset.trans (Metric.ball_subset_ball (min_le_left g1 (r / 2))) hg2
-    exact min_lt_of_right_lt (half_lt_self hr)
-
-  -- jupiiiii
+    · apply lt_min (half_pos hg1) (half_pos hr)
+    · constructor
+      · apply Subset.trans (Metric.closedBall_subset_ball (lt_of_le_of_lt (min_le_left (g1/2) (r/2)) (half_lt_self (hg1)))) hg2
+      · exact min_lt_of_right_lt (half_lt_self hr)
 
 
+lemma exists_nested_balls_sequence {W: Set Y} {U: ℕ → Set Y}
+(hWopen : IsOpen W) (hWne: W.Nonempty) (hUnopen: ∀ n, IsOpen (U n)) (hUdense: ∀ n, Dense (U n)):
+∃ (r_seq: ℕ → ℝ) (x_seq: ℕ → Y), ((∀ n, Metric.closedBall (x_seq (n+1)) (r_seq (n+1)) ⊆ Metric.ball (x_seq n) (r_seq n)
+∧ Metric.closedBall (x_seq 0) (r_seq 0) ⊆ W ∩ U 0) ∧ (∀ n, r_seq n ≤ 1/2^n)) := by
 
-
+  sorry
 
 theorem complete_metric_has_baire_property {f : ℕ → Set Y} (ho : ∀ n, IsOpen (f n))
     (hd : ∀ n, Dense (f n)) : Dense (⋂ n, f n) :=
