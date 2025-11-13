fc720ea452857265d2f30d5eb9f2c84986b94bd0
Monika Brattig <345051@uwr.edu.pl>
2025-11-13 21:23:07 +0100

    spacje!!!!

--- BaireTheoremTwo/Basic.lean
+++ BaireTheoremTwo/Basic.lean
@@ -102,8 +102,8 @@ lemma exist_open_ball_smaller_radius_subset {P : Set Y} {r : ℝ} (hr : 0 < r) (
       · exact min_lt_of_right_lt (half_lt_self hr)
 
 
-lemma exists_nested_balls_sequence {W: Set Y} {U: ℕ → Set Y}
-(hWopen : IsOpen W) (hWne: W.Nonempty) (hUnopen: ∀ n, IsOpen (U n)) (hUdense: ∀ n, Dense (U n)):
+lemma exists_nested_balls_sequence {W : Set Y} {U : ℕ → Set Y}
+(hWopen : IsOpen W) (hWne : W.Nonempty) (hUnopen : ∀ n, IsOpen (U n)) (hUdense : ∀ n, Dense (U n)) :
 ∃ (r_seq: ℕ → ℝ) (x_seq: ℕ → Y), ((∀ n, Metric.closedBall (x_seq (n+1)) (r_seq (n+1)) ⊆ Metric.ball (x_seq n) (r_seq n)
 ∧ Metric.closedBall (x_seq 0) (r_seq 0) ⊆ W ∩ U 0) ∧ (∀ n, r_seq n ≤ 1/2^n)) := by
 
