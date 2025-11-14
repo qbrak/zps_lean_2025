262345aa68f20c82a81f6f5be73a04d6a81da3ed
Piotr Held <pjheld@gmail.com>
2025-11-14 20:31:49 +0100

    Added necessary details to nested ball lemma

--- BaireTheoremTwo/Basic.lean
+++ BaireTheoremTwo/Basic.lean
@@ -4,7 +4,7 @@ import Mathlib.Topology.Basic
 
 import Mathlib.Topology.Closure
 import Mathlib.Topology.MetricSpace.Pseudo.Defs
-
+import Mathlib.Topology.Metrizable.CompletelyMetrizable
 
 
 set_option linter.style.longLine false
@@ -18,7 +18,7 @@ variable {X Y α B B1 : Type*} {ι : Sort*}
 section BaireTheoremTwo
 
 variable [TopologicalSpace X] [BaireSpace X]
-variable [MetricSpace Y] [CompleteSpace Y]
+variable [MetricSpace Y] [CompleteSpace Y] [IsCompletelyMetrizableSpace X]
 --variable [MetricSpace Z]
 
 /--
@@ -102,10 +102,10 @@ lemma exist_open_ball_smaller_radius_subset {P : Set Y} {r : ℝ} (hr : 0 < r) (
       · exact min_lt_of_right_lt (half_lt_self hr)
 
 
-lemma exists_nested_balls_sequence {W : Set Y} {U : ℕ → Set Y}
+lemma exists_nested_balls_sequence {W: Set Y} {U: ℕ → Set Y}
 (hWopen : IsOpen W) (hWne : W.Nonempty) (hUnopen : ∀ n, IsOpen (U n)) (hUdense : ∀ n, Dense (U n)) :
-∃ (r_seq: ℕ → ℝ) (x_seq: ℕ → Y), ((∀ n, Metric.closedBall (x_seq (n+1)) (r_seq (n+1)) ⊆ Metric.ball (x_seq n) (r_seq n)
-∧ Metric.closedBall (x_seq 0) (r_seq 0) ⊆ W ∩ U 0) ∧ (∀ n, r_seq n ≤ 1/2^n)) := by
+∃ (r_seq: ℕ → ℝ) (x_seq: ℕ → Y), (((∀ n, Metric.closedBall (x_seq (n+1)) (r_seq (n+1)) ⊆ Metric.ball (x_seq n) (r_seq n))
+∧ (Metric.closedBall (x_seq 0) (r_seq 0) ⊆ W ∩ U 0)) ∧ (∀ n, 0 < r_seq n ∧  r_seq n ≤ 1/2^n)) := by
 
   sorry
 
@@ -119,12 +119,10 @@ theorem complete_metric_has_baire_property {f : ℕ → Set Y} (ho : ∀ n, IsOp
     --- then use completeness to get a limit point in the intersection
     sorry
 
-def NowhereDense (s : Set X) : Prop :=
-  interior (closure s) = ∅
 
 theorem sum_sets_atleast_one_not_nowhere_dense {f : ℕ → Set Y} [Nonempty Y]
    (hUnion : ⋃ n, f n = univ) :
-    ∃ n, ¬ NowhereDense (f n) := by
+    ∃ n, ¬ IsNowhereDense (f n) := by
   by_contra h_contra
   push_neg at h_contra
   have compl_closure_open : ∀ n, IsOpen (closure (f n))ᶜ := by
